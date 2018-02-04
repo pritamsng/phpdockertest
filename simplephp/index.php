fc77456dc7bc00800de41f4fafa7b4377f7ec8b9
Pritam patel <pritamsng@gmail.com>
2018-02-05 03:20:32 +0530

    updating docker file

--- simplephp/index.php
+++ simplephp/index.php
@@ -17,7 +17,7 @@
             <div class="hero-unit">
                 <h1>Simple PHP App</h1>
                 <h2>Congratulations!</h2>
-                <p>Your PHP application is now running on the host &ldquo;<?php echo gethostname(); ?>&rdquo; in your own dedicated environment in the AWS&nbsp;Cloud.</p>
+                <p>Your PHP application is now running on the Docker host &ldquo;<?php echo gethostname(); ?>&rdquo; in your Test environment of Docker&nbsp;Cloud.</p>
                 <p>This host is running PHP version <?php echo phpversion(); ?>.</p>
             </div>
         </div>
